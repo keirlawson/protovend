0fecf86525b61b7e1b5c11c960c001036e043ea7
Keir Lawson <keirlawson@gmail.com>
2020-01-17 16:34:41 +0000

    windows build work

--- .travis/windows_compile_fix.sh
+++ .travis/windows_compile_fix.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-env
-ls /c/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/
-cp -f /mingw64/x86_64-w64-mingw32/lib/{*.a,*.o} `rustc --print sysroot`/lib/rustlib/x86_64-pc-windows-gnu/lib
+ls /c/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/x86_64-w64-mingw32/lib/
+cp -f /c/ProgramData/chocolatey/lib/mingw/tools/install/mingw64/x86_64-w64-mingw32/lib/{*.a,*.o} `rustc --print sysroot`/lib/rustlib/x86_64-pc-windows-gnu/lib
